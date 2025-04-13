97d4858be81ad01f4e2ace5984f817ad9317ed57
kt-tnbns <tangg6@outlook.com>
2025-04-13 14:42:38 +0700

    chore: update user table

--- solidithai-web/src/pages/user/table/useUserTable.ts
+++ solidithai-web/src/pages/user/table/useUserTable.ts
@@ -1,24 +1,72 @@
+import { useState } from "react"
 import { User } from "../../../types/auth"
+import { DataTableColDef } from "../../../types/dataTable"
+import { useGetUsers } from "../../../api/userApi"
 
 export const useUserTable = () => {
+  const [selectedUser, setSelectedUser] = useState<User | null>(null)
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false)
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
+
+  const { data: users, isLoading } = useGetUsers({
+    page: 1,
+    limit: 10,
+  })
+  
+  const columns: DataTableColDef<User>[] = [
+    {
+      headerName: 'Name',
+      field: 'firstName',
+      sortable: true,
+    },
+    {
+      headerName: 'Email',
+      field: 'email',
+      sortable: true,
+    },
+  ]
+
+  const handleEdit = (user: User) => {
+    setSelectedUser(user)
+    setIsEditModalOpen(true)
+  }
+
+  const handleDelete = (user: User) => {
+    setSelectedUser(user)
+    setIsDeleteModalOpen(true)
+  }
+
+  const handleOnCloseEditModal = () => {
+    setIsEditModalOpen(false)
+    setSelectedUser(null)
+  }
+
+  const handleOnCloseDeleteModal = () => {
+    setIsDeleteModalOpen(false)
+    setSelectedUser(null)
+  }
+
+  const handleEditSubmit = () => {
+    setIsEditModalOpen(false)
+  }
+
+  const handleDeleteSubmit = () => {
+    setIsDeleteModalOpen(false)
+    setSelectedUser(null)
+  }
+
   return {
-    userTable: [
-      {
-        id: '1',
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'john.doe@example.com',
-        createdAt: '2021-01-01',
-        updatedAt: '2021-01-01',
-      },
-      {
-        id: '2',
-        firstName: 'Jane',
-        lastName: 'Doe',
-        email: 'jane.doe@example.com',
-        createdAt: '2021-01-01',
-        updatedAt: '2021-01-01',
-      },
-    ] as User[]
+    columns,
+    handleEdit,
+    handleDelete,
+    handleEditSubmit,
+    handleDeleteSubmit,
+    handleOnCloseEditModal,
+    handleOnCloseDeleteModal,
+    isEditModalOpen,
+    isDeleteModalOpen,
+    selectedUser,
+    users: users?.items || [],
+    isLoading,
   }
 }
